b6fafa8a119fbfeb78088d33599dd4b708cbb169
Godfrey Chan <godfreykfc@gmail.com>
2019-03-27 14:09:18 -0400

    [CLEANUP] Remove ViewRegistry
    The framework does not need it anymore.
    
    In most cases, `Ember.ViewUtils.getElementView` can be used as a
    replacement to get the classic component (`Ember.Component`) that
    corresponds to a DOM element (if any).

--- packages/@ember/application/lib/application.js
+++ packages/@ember/application/lib/application.js
@@ -1102,11 +1102,6 @@ Application.reopenClass({
 
 function commonSetupRegistry(registry) {
   registry.register('router:main', Router.extend());
-  registry.register('-view-registry:main', {
-    create() {
-      return dictionary(null);
-    },
-  });
 
   registry.register('route:basic', Route);
   registry.register('event_dispatcher:main', EventDispatcher);
